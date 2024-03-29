e32c804dfa18e60debdd6f5a93bbc4790d21660b
fc <corral.fdo@gmail.com>
2024-03-29 09:44:56 -0700

    Se agragan archivos de components, helper y el hook, para darle funcionalidad

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import MenuItems from "./components/MenuItems"
+import OrderContents from "./components/OrderContents"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
-  console.log(menuItems);
+
+  const {order, addItem, removeItem} = useOrder()
 
   return (
     <>
@@ -10,11 +14,24 @@ function App() {
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2 >Menu</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-black" >Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItems
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
+
         </div>
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents
+            order={order}
+            removeItem = {removeItem}
+          />
         </div>
       </main>
 
